fb9fa495489817e63b01d0e925ba720475f41639
Alan Robson <alanr@live.com.au>
2023-11-15 21:37:01 +1100

    Remove Kml model and modify Photo model for database layer.

--- PhotoKml/Models/Photo.cs
+++ PhotoKml/Models/Photo.cs
@@ -8,7 +8,9 @@ namespace PhotoKml.Models
 {
     internal class Photo
     {
+        public int KmlId { get; set; }
         public string? Name { get; set; }  // Title
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public string? TimeTaken { get; set; } // Formatted
         public DateTime? CreationTime { get; set; }
